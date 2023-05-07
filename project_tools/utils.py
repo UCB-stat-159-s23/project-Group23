ffaf91296dbcfcaf8e456ca42f52b44f0a8fdaac
Shih-Hung Chiu <shchiu@berkeley.edu>
2023-05-07 23:27:13 +0000

    add function to util

--- project_tools/utils.py
+++ project_tools/utils.py
@@ -3,37 +3,33 @@ import pandas as pd
 import calendar
 import os
 
-
-
 def monthly_weekday_counts(entry_station, exit_station, start_year, end_year):
-	
-	 """
-	 This function generates a list that contains monthly ridership of specified entry/exit stations and start/end years timeframe. 
-	 
+    
+    """
+    This function generates a list that contains monthly ridership of specified entry/exit stations and start/end years timeframe. 
+    
     Parameters
     ----------
     entry_station : str
         This defines the entry_station in its two characters reference name. Ex. "EM" for Embarcadero
-	exit_station : str
+    exit_station : str
         This defines the exit_station in its two characters reference name. Ex. "EM" for Embarcadero
-	start_year : int
+    start_year : int
         This defines the start year of the timeframe, starting from January.
-	end_year : int
+    end_year : int
         This defines the end year of the timeframe, ending in December.
-	
-	Returns
-	----------
-	monthly_weekday_counts : list[double]
+
+    Returns
+    ----------
+    monthly_weekday_counts : list[double]
         This returns the monthly weekday counts.
-	monthly_weekday_plot_label : list[str]
+    monthly_weekday_plot_label : list[str]
         This returns a label for the corresponding index's month in the count list. format: {year}-{two_digits_format_month}
     """
-	
-	
-	
+        
     monthly_weekday_counts = []
     monthly_weekday_plot_label = []
-
+    
     for year in np.arange(start_year, end_year+1, 1):
         dir_path = f'Data/ridership_{year}/'
         if year <= 2017:
@@ -52,3 +48,43 @@ def monthly_weekday_counts(entry_station, exit_station, start_year, end_year):
                 monthly_weekday_plot_label.append(f'{year}-{two_digits_format_month}')
                 
     return monthly_weekday_counts, monthly_weekday_plot_label
+
+def annual_arrival_trips(year, station):
+    
+    '''
+    This function generates a list that contains the annual arrival ridership of the given stations. 
+    
+    Parameters
+    ----------
+    year: (list of str)
+        A list of the years (years after 2018)
+    station: (list of str)
+        A list of stations that are under interest
+    
+    Returns
+    ----------
+    Output: pandas dataframe
+        dataframe of the arrival riderships given the station and years
+    '''
+    
+    output = pd.DataFrame(columns = station)
+    
+    for j in range(len(year)):
+        file_dir = 'Data/ridership_' + year[j] + '/'
+        file_name = []
+        
+        for i in range(12):
+            if i < 9:
+                string = 'Ridership_'+year[j]+'0'+str(i+1)
+            else:
+                string = 'Ridership_'+year[j]+str(i+1)
+            file_name.append(string)
+        
+            string = file_dir + file_name[i] + ".xlsx"
+            temp = pd.read_excel(string, skiprows = 1)
+            temp = temp.set_index('Unnamed: 0')
+            output.loc[file_name[i][-6:]] = temp.loc['Entries'][station]
+    
+    return output
+
+
