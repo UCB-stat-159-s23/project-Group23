21fab43992d3a245c29521bf96d7a02bcb929059
Noor Wahle <noorwahle@gmail.com>
2023-05-09 22:17:24 +0000

    Add mapping functions tests

--- project_tools/test/test_utils.py
+++ project_tools/test/test_utils.py
@@ -10,3 +10,19 @@ def test_monthly_weekday_counts():
 def test_annual_arrival_count():
     output = annual_arrival_trips(year = ['2022'], station = ['EM','MT', 'PL', 'CC'])
     assert isinstance(output, pd.DataFrame)
+
+
+def test_calc_stn_perc_diffs():
+    output = calc_stn_perc_diffs(SF_pre_covid, SF_post_covid)
+    assert isinstance(output, pd.DataFrame)
+
+def test_add_markers_to_map():
+    berk_map = folium.Map(location=[37.8715, -122.2730], tiles="cartodbpositron", zoom_start=15)
+    berk_mapping_df = pd.DataFrame({
+        'lon': [-122.2681, -122.2834],
+        'lat': [37.8701, 37.8740],
+        'name': ['Downtown Berkeley: '+str(round(berk_percent_diffs[0]))+'%',
+                'North Berkeley: '+str(round(berk_percent_diffs[1]))+'%'],
+        'value': berk_percent_diffs}, dtype=str)
+    output = add_markers_to_map(berk_mapping_df, 6, 'teal', berk_map)
+    assert isinstance(output, folium.folium.Map)
